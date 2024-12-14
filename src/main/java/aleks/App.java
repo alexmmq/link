7d68b0174eb8c944c6578d6eb60ee3f94b564097
alexmmqb <alexmmqb@gmail.com>
2024-12-15 01:22:17 +0300

    working random string in main

--- src/main/java/aleks/App.java
+++ src/main/java/aleks/App.java
@@ -11,6 +11,21 @@ public class App
     public static void main( String[] args )
     {
         UUID uuid = UUID.randomUUID();
+        String longLink = "https://stackoverflow.com/questions/17984975/convert-int-to-char-in-java";
+        String link = "http://short/";
         System.out.println( uuid );
+        System.out.println((int)(Math.random()*10));
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(link);
+        for(int i = 0; i < 8; i++){
+            int a,b, average, random, result;
+            a = uuid.toString().charAt(i);
+            b = longLink.charAt(longLink.length() - 1 - i);
+            average = (a + b) / 2;
+            random = (int)(Math.random() * 10);
+            result = random + average;
+            stringBuilder.append((char)result);
+        }
+        System.out.println(stringBuilder.toString());
     }
 }
--- src/main/java/aleks/services/LinkServiceImpl.java
+++ src/main/java/aleks/services/LinkServiceImpl.java
@@ -9,9 +9,24 @@ public class LinkServiceImpl implements LinkService{
     @Override
     public String createAShortLink(String longLink, User user) {
         LinkControllerImpl linkController = new LinkControllerImpl();
+        //create a method of generating a random string - iterate through UUID, get first char,
+        //get last char of long link, convert both to int, calculate average, add Math.random()*10
+        //take this as a one char for the new link
+        String link = "https://link/";
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(link);
+        for(int i = 0; i < 8; i++){
+            int a,b, average, random, result;
+            a = user.getUUID().toString().charAt(i);
+            b = longLink.charAt(longLink.length() - 1 - i);
+            average = (a + b) / 2;
+            random = (int)(Math.random() * 10);
+            result = random + average;
+            stringBuilder.append((char)result);
+        }
 
         //invoke a create entry method of LinkController
-        return "";
+        return stringBuilder.toString();
     }
 
     @Override
